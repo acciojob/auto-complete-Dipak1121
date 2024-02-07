54f1be980ce158253f4e57b95499714473d84748
Dipak1121 <dipaks9604@gmail.com>
2024-02-07 15:12:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,50 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
-  return (
+
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+  const [resultArr, setResultArr] = useState(fruits);
+  
+  const [text, setText] = useState("");
+
+
+  function handleChange(e){
+
+    setText(e.target.value);
+
+    if(!text){
+      setResultArr(fruits);
+    }
+    else{
+      setResultArr(fruits.filter((item)=>{
+        if(item.toLowerCase().includes(text.trim().toLowerCase())){
+          return true;
+        }
+        return false;
+      }))
+    }
+
+    console.log(resultArr);
+  }
+
+   return (
     <div>
-        {/* Do not remove the main div */}
+        <input type="text"
+        onChange={handleChange}
+        value={text}></input>
+        <ul>
+          {
+            resultArr.map((item, index)=>{
+              return(
+                <li key={index}>{item}</li>
+              )
+            })
+          }
+        </ul>
     </div>
   )
 }
